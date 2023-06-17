3326a57f88a1fd084a36cbaa4e925235a3b37e3f
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2023-06-17 13:50:09 -0600

    axios4

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import { useState } from "react";
 import Alerta from "../components/Alerta";
+import axios from 'axios';
 import clienteAxios from '../config/axios';
 
 const Login = () => {
